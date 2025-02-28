8932d75a78087188fa7d9058140662f5deb52645
Samar AYADI <ayadisamardev@gmail.com>
2025-03-01 00:34:04 +0100

    Defining App Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginForm from "./pages/Auth/LoginForm";
+import SignUpForm from "./pages/Auth/SignUpForm";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signUp" element={<SignUpForm />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+// Define the Root component to handle the initial redirect
+const Root = () => {
+  // Check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // Redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
+
+export default App;
