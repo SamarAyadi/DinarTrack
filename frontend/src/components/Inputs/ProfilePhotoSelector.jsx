56e96721b2ce4c9159cb628c12ef5de42ed69de1
Samar AYADI <ayadisamardev@gmail.com>
2025-03-01 01:53:35 +0100

    Building the Sign-Up Form UI

---
+++ frontend/src/components/Inputs/ProfilePhotoSelector.jsx
@@ -0,0 +1,17 @@
+import React, { useRef, useState } from 'react'
+
+const ProfilePhotoSelector = ({ image, setImage }) => {
+
+  const inputRef = useRef(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ProfilePhotoSelector
